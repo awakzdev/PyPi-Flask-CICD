ba1eefab201764a9688631b0738dff6ddfdd0f62
ellie hodjayev <zenmyx@gmail.com>
2022-07-12 19:04:57 +0000

    Add new file

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+cd /etc
+touch pip.conf
+echo "[global]
+index-url = https://zenmyx@gmail.com:AKCp8mZccd9KLfTUfZkz1sxVx24ZqUDj43uWgoSD9xN1bHmMh3YHQVYtwzbQox422xgTth27L@awakzdev.jfrog.io/artifactory/api/pypi/pypi/simple" > pip.conf
