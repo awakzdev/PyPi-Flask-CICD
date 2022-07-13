ee4b8d17f8307d5701f4949880b517d0e9eee43a
ellie hodjayev <zenmyx@gmail.com>
2022-07-13 09:19:50 +0000

    Update simpleflask.py

--- simpleflask.py
+++ simpleflask.py
@@ -1,14 +1,16 @@
-from flask import Flask
-
-
+from flask import Flask, jsonify
+from ganesha_experimental.main import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/Prime/', methods=['GET'])
+def Prime():
+    n = random.randint(2, 100)
+    previous_primes_list = printPrime(n)
+    print(previous_primes_list)
 
+    return jsonify({'Prime', previous_primes_list})
 
 
 if __name__ == "__main__":
